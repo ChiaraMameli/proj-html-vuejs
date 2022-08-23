f0fbe1929ca6e0b0bfd964af566ff166dbe52f34
ChiaraMameli <mameli.chiara@libero.it>
2022-08-23 18:02:33 +0200

    added Font Awesome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faHatWizard } from '@fortawesome/free-solid-svg-icons'
+library.add(faHatWizard)
+Vue.component('FontAwesomeIcon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
