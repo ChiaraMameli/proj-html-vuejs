2ef5725687a85d97be4a193a6af4bd02456b4cc6
ChiaraMameli <mameli.chiara@libero.it>
2022-08-23 18:55:39 +0200

    added details to header

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -1,16 +1,16 @@
 <template>
   <nav class="container d-flex justify-content-between">
-    <div><a href="#"><img src="../assets/img/logo-default.png" alt="iAcademy Logo"></a></div>
+    <div id="header-logo"><a href="#"><img src="../assets/img/logo-default.png" alt="iAcademy Logo"></a></div>
     <div>
-        <ul class="d-flex">
-            <li v-for="item in NavLinks" :key="item.label"><a :href="item.link" class="text-white text-uppercase">{{item.label}}</a></li>
+        <ul  class="d-flex align-items-center h-100">
+            <li v-for="item in NavLinks" :key="item.label"><a :href="item.link" class="text-white text-uppercase p-2">{{item.label}}</a></li>
         </ul>
     </div>
     <div>
-        <ul class="d-flex">
-            <li>i</li>
-            <li>i</li>
-            <li>i</li>
+        <ul id="header-menu-right" class="d-flex align-items-center h-100">
+            <li><a href="#" class="text-white ps-4"><FontAwesomeIcon icon="fa-solid fa-magnifying-glass" /></a></li>
+            <li><a href="#" class="text-white ps-4"><FontAwesomeIcon icon="fa-solid fa-bag-shopping" /></a></li>
+            <li><a href="#" class="text-white ps-4"><FontAwesomeIcon icon="fa-solid fa-bars" /></a></li>
         </ul>
     </div>
   </nav>
@@ -27,5 +27,15 @@ export default {
 
 <style scoped lang="scss">
 @import '../assets/sass/style.scss';
+nav{
+    padding: 2rem;
+
+    #header-logo{
+        img{ 
+            filter: invert(100%) sepia(54%) saturate(2%) hue-rotate(181deg) brightness(250%) contrast(101%);
+            height: 2rem;
+        }
+    }
+}
 
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faHatWizard } from '@fortawesome/free-solid-svg-icons'
-library.add(faHatWizard)
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faBagShopping } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+
+library.add(faMagnifyingGlass, faBagShopping, faBars)
 Vue.component('FontAwesomeIcon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
