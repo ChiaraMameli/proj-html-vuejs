60740e9be36a3b514a7ea5a1bc7fa46edda48b29
ChiaraMameli <mameli.chiara@libero.it>
2022-08-24 11:59:20 +0200

    addded section LearningFieldsCards

---
+++ src/components/BaseMain.vue
@@ -0,0 +1,53 @@
+<template>
+<div>
+    <LearningFieldsCards :LearningFields="LearningFields" />
+</div>
+</template>
+
+<script>
+import LearningFieldsCards from './LearningFieldsCards.vue';
+export default {
+    name: "BaseMain",
+    data() {
+        return {
+            LearningFields: [
+                {
+                    label: "languages",
+                    imgUrl: "globe",
+                    id: 1,
+                },
+                {
+                    label: "software",
+                    imgUrl: "pen",
+                    id: 2,
+                },
+                {
+                    label: "business",
+                    imgUrl: "case",
+                    id: 3,
+                },
+                {
+                    label: "chemistry",
+                    imgUrl: "test-tubes",
+                    id: 4,
+                },
+                {
+                    label: "science",
+                    imgUrl: "rocket",
+                    id: 5,
+                },
+                {
+                    label: "DIY&Craft",
+                    imgUrl: "scissors",
+                    id: 6,
+                },
+            ]
+        };
+    },
+    components: { LearningFieldsCards }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/LearningFieldsCards.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container pt-5">
+    <div class="d-flex justify-content-between">
+        <div v-for="field in LearningFields" :key="field.id" class="_card p-4">
+            <img :src="require(`../assets/img/${field.imgUrl}.png`)" :alt="field.label" class="p-4">
+            <h5 class="p-4">{{field.label}}</h5>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'LearningFieldsCards',
+    props:{
+        LearningFields: Array
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../assets/sass/colours';
+._card{
+    background-color: $light_blue;
+    border-radius: 0.5rem;
+}
+</style>
