67bf520ecd8582afb62631d07037b05c94210d7b
ChiaraMameli <mameli.chiara@libero.it>
2022-08-25 11:52:29 +0200

    styled SectionArticle

--- src/components/SectionArticle.vue
+++ src/components/SectionArticle.vue
@@ -1,19 +1,19 @@
 <template>
   <section class="container pt-5 pb-5">
     <div class="row">
-        <div class="col-4">
+        <div class="col-3">
             <div v-for="link in ArticleLinks" :key="link.id" class="slot">
                 <span>{{link.label}}</span>
             </div>
         </div>
-        <article class="col-8">
-            <h3 class="pb-2">Learning Possibilities</h3>
-            <span class="d-block pb-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis soluta esse iusto minima numquam explicabo reprehenderit fugiat! Modi hic ipsum voluptas, debitis vero saepe! Inventore commodi nemo totam quisquam ratione.</span>
+        <article class="col-9">
+            <h4 class="pb-4">Learning Possibilities</h4>
+            <span class="d-block pb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis soluta esse iusto minima numquam explicabo reprehenderit fugiat! Modi hic ipsum voluptas, debitis vero saepe! Inventore commodi nemo totam quisquam ratione.</span>
             <ul>
-                <li><FontAwesomeIcon class="check-icon" icon="fa-solid fa-check" /> We enrich lives through learning</li>
-                <li><FontAwesomeIcon class="check-icon" icon="fa-solid fa-check" /> Maximizing potential through individual attention</li>
-                <li><FontAwesomeIcon class="check-icon" icon="fa-solid fa-check" /> The trusted Name for specialized training</li>
-                <li><FontAwesomeIcon class="check-icon" icon="fa-solid fa-check" /> People teach. People learn. This is how they connect.</li>
+                <li class="pb-4"><FontAwesomeIcon class="check-icon" icon="fa-solid fa-check" /> We enrich lives through learning</li>
+                <li class="pb-4"><FontAwesomeIcon class="check-icon" icon="fa-solid fa-check" /> Maximizing potential through individual attention</li>
+                <li class="pb-4"><FontAwesomeIcon class="check-icon" icon="fa-solid fa-check" /> The trusted Name for specialized training</li>
+                <li class="pb-4"><FontAwesomeIcon class="check-icon" icon="fa-solid fa-check" /> People teach. People learn. This is how they connect.</li>
 
             </ul>
         </article>
@@ -58,34 +58,43 @@ export default {
 
 <style scoped lang="scss">
 @import '../assets/sass/colours';
-    .slot{
-        width: 20rem;
-        padding: 1.5rem;
-        border: 1px solid $grey;
-
-        &:hover{
-            cursor: pointer;
-            color: $primary_colour;
-            border-left: 5px solid $primary_colour;
-        }
-    }
-
-    article{
-
-        h3{
-            font-size: 2.9rem;
-            color: $dark_grey;
-        }
-
-        span, li{
-        color: $grey;
-        }
-
-        .check-icon{
-            color: $primary_colour;
-        }
+@import '../assets/sass/utils';
+    section{
+        margin-top: 5rem;
+        margin-bottom: 5rem;
         
-    }
+        .slot{
+            width: 18rem;
+            padding: 1.5rem;
+            border: 1px solid $border_colour;
+            font-weight: 600;
+            color: $span_colour;
 
+            &:hover{
+                cursor: pointer;
+                color: $primary_colour;
+                border-left: 5px solid $primary_colour;
+                background-color: $fill_colour;
+            }
+        }
+
+        article{
+
+            h4{
+                font-size: $h4_size;
+                color: $h4_colour;
+                font-weight: 700;
+            }
+
+            span, li{
+            color: $span_colour;
+            }
+
+            .check-icon{
+                color: $primary_colour;
+            }
+            
+        }
+    }
 
 </style>
